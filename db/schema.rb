f4ac4dd4a11cad22f6cbb78430dd5f4bf7c7edf4
albert <albert3787@gmail.com>
2022-04-19 21:04:49 +0800

    add product model and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_04_19_122509) do
+ActiveRecord::Schema.define(version: 2022_04_19_130324) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "picture"
+    t.text "description"
+    t.string "status"
+    t.string "upload_time"
+    t.integer "direct_price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
