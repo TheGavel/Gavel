c6a3cfb1eedb8438e074c1adf78474635506a9d4
weian <kevin19960328@gmail.com>
2022-04-26 18:39:16 +0800

    add customer_service.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,6 @@ Rails.application.routes.draw do
       get :activate
     end
   end
-
   get 'login' => 'user_sessions#new', :as => :login
   post 'login' => "user_sessions#create"
   post 'logout' => 'user_sessions#destroy', :as => :logout
