9592032a7222be74be1cbf175bee0e1f49e0c54d
alice <yamap19950521@gmail.com>
2022-04-27 18:41:21 +0800

    confign

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get 'login' => 'user_sessions#new', :as => :login
   post 'login' => "user_sessions#create"
   post 'logout' => 'user_sessions#destroy', :as => :logout
+  
 
 
 end
