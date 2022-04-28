651efd8fd1adc581fa06661626f7a9018ac80155
cin-yi-wei <m95162027@gmail.com>
2022-04-28 13:46:02 +0800

    add react

--- app/javascript/css/tailwind.js
+++ app/javascript/css/tailwind.js
@@ -1,11 +1,52 @@
+// "./app/helpers/**/*.rb",
+// "./app/javascript/**/*.js?",
+// "./app/views/**/*.{erb,haml,html,slim}",
+const plugin = require("tailwindcss/plugin");
+
 module.exports = {
-  content: [
-    "./app/helpers/**/*.rb",
-    "./app/javascript/**/*.js?",
-    "./app/views/**/*.{erb,haml,html,slim}",
+  mode: "jit",
+  purge: [
+    "app/helpers/**/*.rb",
+    "app/javascript/**/*.jsx",
+    "app/views/**/*.{erb,haml,html,slim}",
   ],
+  variants: {
+    extend: {
+        borderStyle: ["responsive", "hover"],
+        fontSize: ['responsive',"group-hover"], //加入 group-hover
+    },
+},
   theme: {
-    extend: {},
+    tabSize: {
+      1: '1',
+      2: '2',
+      "30px": "30",
+      "36px": '32px'
+    },
+    extend: {
+      gridTemplateColumns: {
+        'auto-fit': 'repeat(auto-fit, minmax(0, 1fr))',
+        'auto-fill': 'repeat(auto-fill, minmax(0, 1fr))',
+        'fit-250' : 'repeat(auto-fit, minmax(250px , 1fr))',
+        // size: var(--fontSize);
+      },
+      gridTemplateRows: {
+        'auto-fit': 'repeat(auto-fit, minmax(0, 1fr))',
+        'auto-fill': 'repeat(auto-fill, minmax(0, 1fr))',
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+  plugin(function({ matchUtilities, theme }) {
+    matchUtilities(
+      {
+        gridfit: (value) => ({
+          "grid-template-columns": `repeat(auto-fit, minmax(${value}, 1fr))`
+        }),
+      },
+      { values: theme('width') }
+    )
+  })
+
+],
 };
