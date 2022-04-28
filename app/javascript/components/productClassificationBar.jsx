28bb42acf416949e363526eac2fde0566c09f3e1
cin-yi-wei <m95162027@gmail.com>
2022-04-28 20:32:14 +0800

    add product classification bar conponents

---
+++ app/javascript/components/productClassificationBar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Hello = () =>{
+  return (
+    <div className="my-5 tracking-wider font-medium text-3xl  flex justify-around navbar relative">
+      <h3 className="hover">xxx</h3>
+      <h3 className="hover">xxx</h3>
+      <h3 className="hover">xxx</h3>
+      <h3 className="hover">xxx</h3>
+      <h3 className="hover">xxx</h3>
+      <h3 className="hover">xxx</h3>
+      <h3 className="hover">xxx</h3>
+      <h3 className="hover">xxx</h3>
+    </div>
+  )
+}
+export default() => Hello()
