4853ffc50d10e28e2e81ca3fc0e367dc6f6b602f
cin-yi-wei <m95162027@gmail.com>
2022-04-29 00:04:12 +0800

    Remove useless annotations

--- app/javascript/css/tailwind.js
+++ app/javascript/css/tailwind.js
@@ -1,38 +1,22 @@
-// "./app/helpers/**/*.rb",
-// "./app/javascript/**/*.js?",
-// "./app/views/**/*.{erb,haml,html,slim}",
-
-// "app/helpers/**/*.rb",
-// "app/javascript/**/*.jsx",
-// "app/views/**/*.{erb,haml,html,slim}",
 const plugin = require("tailwindcss/plugin");
 
 module.exports = {
   mode: "jit",
   purge: [
-"app/helpers/**/*.rb",
-"app/javascript/**/*.jsx",
-"app/views/**/*.{erb,haml,html,slim}",
+    "app/helpers/**/*.rb",
+    "app/javascript/**/*.jsx",
+    "app/views/**/*.{erb,haml,html,slim}",
   ],
   variants: {
     extend: {
-        borderStyle: ["responsive", "hover"],
-        fontSize: ['responsive',"group-hover"], //加入 group-hover
     },
 },
   theme: {
-    tabSize: {
-      1: '1',
-      2: '2',
-      "30px": "30",
-      "36px": '32px'
-    },
     extend: {
       gridTemplateColumns: {
         'auto-fit': 'repeat(auto-fit, minmax(0, 1fr))',
         'auto-fill': 'repeat(auto-fill, minmax(0, 1fr))',
         'fit-250' : 'repeat(auto-fit, minmax(250px , 1fr))',
-        // size: var(--fontSize);
       },
       gridTemplateRows: {
         'auto-fit': 'repeat(auto-fit, minmax(0, 1fr))',
@@ -41,16 +25,14 @@ module.exports = {
     },
   },
   plugins: [
-  plugin(function({ matchUtilities, theme }) {
-    matchUtilities(
-      {
-        gridfit: (value) => ({
-          "grid-template-columns": `repeat(auto-fit, minmax(${value}, 1fr))`
-        }),
-      },
-      { values: theme('width') }
-    )
-  })
-
-],
+    plugin(function({ matchUtilities, theme }) {
+      matchUtilities({
+          gridfit: (value) => ({
+            "grid-template-columns": `repeat(auto-fit, minmax(${value}, 1fr))`
+          }),
+        },
+        { values: theme('width') }
+      )
+    })
+  ],
 };
