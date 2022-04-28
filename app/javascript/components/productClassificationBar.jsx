d040e886f91288560ef0eaf72e81cae5f6767482
cin-yi-wei <m95162027@gmail.com>
2022-04-28 23:14:29 +0800

    content replace props

--- app/javascript/components/productClassificationBar.jsx
+++ app/javascript/components/productClassificationBar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Hello = () =>{
+
   return (
     <div className="my-5 tracking-wider font-medium text-3xl  flex justify-around navbar relative">
       <h3 className="hover">xxx</h3>
@@ -14,4 +15,4 @@ const Hello = () =>{
     </div>
   )
 }
-export default() => Hello()
+export default Hello;
