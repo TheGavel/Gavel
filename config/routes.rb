24a13e098ef00a0f9c2782f3de4c3b835ecc9954
albert <albert3787@gmail.com>
2022-05-03 01:05:18 +0800

    add product CRUD

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,6 @@ Rails.application.routes.draw do
   get 'login' => 'user_sessions#new', :as => :login
   post 'login' => "user_sessions#create"
   post 'logout' => 'user_sessions#destroy', :as => :logout
+
+  resources :products
 end
