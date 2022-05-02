1fb220e53525cadeff3aa38959a7cda7ca257192
albert <albert3787@gmail.com>
2022-05-03 01:05:18 +0800

    add product CRUD

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,55 @@
+class ProductsController < ApplicationController
+    before_action :find_product, only: %i[show edit update destroy]
+
+    def index
+        @products = Product.all
+    end
+
+    def show
+        
+    end
+    
+    
+    def new
+        @product = Product.new
+    end
+    
+    def create
+        @product = Product.new(product_params)
+        if @product.save
+            redirect_to products_path, notice: 'Create Sussess!!'
+          else
+            render :new
+          end
+    end
+
+    def edit
+        
+    end
+    
+
+    def update
+        if @product.update(product_params)
+            redirect_to products_path, notice: "Update Sussess!!"
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        @product.destroy
+        redirect_to products_path, notice: "Delete Sussess!!"
+        
+    end
+    
+    private
+
+    def product_params
+        params.require(:product).permit(:name,:description,:direct_price,:upload_time,:status,:picture)
+        
+    end
+    
+    def find_product
+        @product = Product.find_by(id: params[:id])
+    end
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,19 @@
 # frozen_string_literal: true
 
 class Product < ApplicationRecord
+    validates :name , presence: true
+    validates :picture , presence: true
+    validates :description, presence: true, length: { minimum: 10 }
+    validates :upload_time , presence:true
+    validates :direct_price , presence:true,numericality: { greater_than_or_equal_to: 0,  only_integer: true }
+    
+
+
+    
+  def self.all_status
+    [
+      %w[待拍賣 for_sale],
+      %w[立即競標 sales]
+    ]
+  end
 end
