7444ff139264d053f7c4b5577d46b043180f6488
albert <albert3787@gmail.com>
2022-05-04 12:37:11 +0800

    add del confirm, merge same validate in product model

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,51 +1,49 @@
 class ProductsController < ApplicationController
-    before_action :find_product, only: %i[show edit update destroy]
+  before_action :find_product, only: %i[show edit update destroy]
 
-    def index
-        @products = Product.all
-    end
+  def index
+    @products = Product.all
+  end
 
-    def show    
-    end
+  def show    
+  end
     
-    def new
-        @product = Product.new
-    end
+  def new
+    @product = Product.new
+  end
     
-    def create
-        @product = Product.new(product_params)
-        if @product.save
-            redirect_to products_path, notice: 'Create Sussess!!'
-          else
-            render :new
-          end
-    end
+  def create
+    @product = Product.new(product_params)
+      if @product.save
+        redirect_to products_path, notice: 'Create Sussess!!'
+      else
+        render :new
+      end
+  end
 
-    def edit 
-    end
+  def edit 
+  end
     
-    def update
-        if @product.update(product_params)
-            redirect_to products_path, notice: "Update Sussess!!"
-        else
-            render :edit
-        end
+  def update
+    if @product.update(product_params)
+      redirect_to products_path, notice: "Update Sussess!!"
+    else
+      render :edit
     end
+  end
 
-    def destroy
-        @product.destroy
-        redirect_to products_path, notice: "Delete Sussess!!"
-        
-    end
+  def destroy
+    @product.destroy
+    redirect_to products_path, notice: "Delete Sussess!!" 
+  end
     
-    private
+  private
 
-    def product_params
-        params.require(:product).permit(:name,:description,:start_price,:direct_price,:upload_time,:status,:picture)
-        
-    end
+  def product_params
+    params.require(:product).permit(:name,:description,:start_price,:direct_price,:upload_time,:status,:picture)
+  end
     
-    def find_product
-        @product = Product.find_by(id: params[:id])
-    end
+  def find_product
+    @product = Product.find_by(id: params[:id])
+  end
 end
