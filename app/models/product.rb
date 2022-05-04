0da6f3091a0e8d2cbb295255d83110bbf7beb189
albert <albert3787@gmail.com>
2022-05-04 21:33:10 +0800

    replace find-by to find and del space

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class Product < ApplicationRecord
-    validates :name ,:picture , presence: true
-    validates :description, presence: true, length: { minimum: 10 }
-    validates :upload_time , presence:true
-    validates :start_price,:direct_price , presence:true,numericality: { greater_than_or_equal_to: 0,  only_integer: true }
+  validates :name ,:picture , presence: true
+  validates :description, presence: true, length: { minimum: 10 }
+  validates :upload_time , presence:true
+  validates :start_price, :direct_price , presence:true,numericality: { greater_than_or_equal_to: 0,  only_integer: true }
 
     
   def self.all_status
