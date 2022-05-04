df6067bfc16be1555a2f8c0b0254e3fae498b97f
albert <albert3787@gmail.com>
2022-05-04 12:37:11 +0800

    add del confirm, merge same validate in product model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,11 @@
 # frozen_string_literal: true
 
 class Product < ApplicationRecord
-    validates :name , presence: true
-    validates :picture , presence: true
+    validates :name ,:picture , presence: true
     validates :description, presence: true, length: { minimum: 10 }
     validates :upload_time , presence:true
-    validates :start_price , presence:true,numericality: { greater_than_or_equal_to: 0,  only_integer: true }
-    validates :direct_price , presence:true,numericality: { greater_than_or_equal_to: 0,  only_integer: true }
+    validates :start_price,:direct_price , presence:true,numericality: { greater_than_or_equal_to: 0,  only_integer: true }
+
     
   def self.all_status
     [
