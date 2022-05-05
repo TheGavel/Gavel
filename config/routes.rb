97933d289271924d0013b4278d5689582c7e197b
cin-yi-wei <m95162027@gmail.com>
2022-05-05 10:59:00 +0800

    modify route

--- config/routes.rb
+++ config/routes.rb
@@ -7,14 +7,21 @@ Rails.application.routes.draw do
   get 'user_sessions/destroy'
   root "users#index"
 
+  # root "users#index"
+  root "products#index"
   resources :users do
     member do
       get :activate
     end
+
   end
   get 'login' => 'user_sessions#new', :as => :login
   post 'login' => "user_sessions#create"
   post 'logout' => 'user_sessions#destroy', :as => :logout
 
-  resources :products
+  resources :products do
+    collection do
+      get :own
+    end
+  end
 end
