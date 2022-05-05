8a37061ade2fcb5d251a811aa958441b6b81b0b7
weian <kevin19960328@gmail.com>
2022-05-05 17:57:49 +0800

    add_env

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-
+Dotenv::Railtie.load
 module Garvel
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
