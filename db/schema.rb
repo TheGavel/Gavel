030d3f044c9ce5d96297f5b31c8b075acf36f6e4
cin-yi-wei <m95162027@gmail.com>
2022-05-09 21:23:43 +0800

    modify rooms table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_06_031439) do
+ActiveRecord::Schema.define(version: 2022_05_09_105024) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,13 +51,14 @@ ActiveRecord::Schema.define(version: 2022_05_06_031439) do
   end
 
   create_table "rooms", force: :cascade do |t|
-    t.string "countdown"
-    t.string "start_time"
+    t.string "life_time"
+    t.datetime "start_time"
     t.string "status"
-    t.string "level"
     t.integer "maxpeople"
+    t.integer "product_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_rooms_on_product_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -83,4 +84,5 @@ ActiveRecord::Schema.define(version: 2022_05_06_031439) do
   add_foreign_key "records", "products"
   add_foreign_key "records", "rooms"
   add_foreign_key "records", "users"
+  add_foreign_key "rooms", "products"
 end
