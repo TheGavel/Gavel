ec2913c47d8deacf79c9f939d39fa0b05cce3d9c
cin-yi-wei <m95162027@gmail.com>
2022-05-14 21:31:00 +0800

    add multiple images on product model fix room model migrate dependency problem

--- db/migrate/20220509103607_drop_room.rb
+++
@@ -1,5 +0,0 @@
-class DropRoom < ActiveRecord::Migration[6.1]
-  def change
-    drop_table :rooms
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2022_05_11_024509) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
-    t.integer "record_id", null: false
-    t.integer "blob_id", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
     t.datetime "created_at", null: false
     t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
     t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
@@ -35,13 +38,10 @@ ActiveRecord::Schema.define(version: 2022_05_11_024509) do
   end
 
   create_table "active_storage_variant_records", force: :cascade do |t|
-    t.integer "blob_id", null: false
+    t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
 
   create_table "authentications", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -64,15 +64,6 @@ ActiveRecord::Schema.define(version: 2022_05_11_024509) do
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
-  create_table "products_tags", force: :cascade do |t|
-    t.integer "product_id", null: false
-    t.integer "tag_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["product_id"], name: "index_products_tags_on_product_id"
-    t.index ["tag_id"], name: "index_products_tags_on_tag_id"
-  end
-
   create_table "records", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.integer "bid"
@@ -92,16 +83,10 @@ ActiveRecord::Schema.define(version: 2022_05_11_024509) do
     t.datetime "start_time"
     t.string "status"
     t.integer "maxpeople"
-    t.integer "product_id", null: false
+    t.bigint "product_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["product_id"], name: "index_rooms_on_product_id"
-  end
-
-  create_table "tags", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
