851c333b2dff1e1337824410ab03b9615d4f2fd8
yamap19950521 <yamap19950521@gmail.com>
2022-05-15 20:12:18 +0800

    Update user.rb
    已修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,11 @@ class User < ApplicationRecord
   has_many :authentications, :dependent => :destroy
   accepts_nested_attributes_for :authentications
 
-  validates :password, length: { minimum: 6 },
-    if: -> { new_record? || changes[:crypted_password] }
-  validates :password, confirmation: true,
+  
+  validates :password, confirmation: true,length: { minimum: 6 }
     if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true,
     if: lambda {new_record? || changes[:crypted_password]}
-  validates :email, uniqueness: true, presence: true
-  validates :username, uniqueness: true, presence: true
+  validates :email, :username, uniqueness: true, presence: true
   has_many :products
 end
