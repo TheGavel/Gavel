97a554184d6299e7baeb600b948881e2641ec418
livia <yamap19950521@gmail.com>
2022-05-15 13:23:08 +0800

    sign_up updata  and validates

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,6 +3,8 @@ class ProductsController < ApplicationController
   before_action :find_product, only: %i[show edit update destroy]
   before_action :pundit
   rescue_from Pundit::NotAuthorizedError, with: :no_permission
+
+
   def index
     @products = Product.all
   end
