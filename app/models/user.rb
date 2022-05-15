cd27f48e57857395c8c28feaf427f81d4c81947f
livia <yamap19950521@gmail.com>
2022-05-15 13:23:08 +0800

    sign_up updata  and validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,13 @@ class User < ApplicationRecord
   has_many :authentications, :dependent => :destroy
   accepts_nested_attributes_for :authentications
 
-  validates :password, length: { minimum: 3 },
+  validates :password, length: { minimum: 6 },
     if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true,
     if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true,
     if: lambda {new_record? || changes[:crypted_password]}
-  validates :email, uniqueness: true
-
+  validates :email, uniqueness: true, presence: true
+  validates :username, uniqueness: true, presence: true
   has_many :products
 end
