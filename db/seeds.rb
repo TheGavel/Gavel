59d77ae5188e12e700edfc918771209ca5130527
zoe <chaonbie@gmail.com>
2022-05-17 18:18:11 +0800

    Create the seeds of tag

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Tag.all.delete_all
+Tag.create([{ name: '3c' }, { name: '手機' }, { name: 'iphone' }, { name: 'samsung' }, { name: '運動' }, { name: '跑步機' }, { name: '喬山' }, { name: 'iphone11' }, { name: 'android' }])
