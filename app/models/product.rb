063ee7428e0fed47112fdffbdab2e5c4ee946ebe
livia <yamap19950521@gmail.com>
2022-05-17 14:57:50 +0800

    add_search

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,6 @@ class Product < ApplicationRecord
   searchkick  searchable: [:name],
               word_middle: [:name],
               callbacks: :async
-
   validates :name , presence: true
   validates :description, presence: true, length: { minimum: 10 }
   validates :start_price, :direct_price , presence:true,numericality: { greater_than_or_equal_to: 0,  only_integer: true }
