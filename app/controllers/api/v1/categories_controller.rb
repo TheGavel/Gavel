2cf8310882f92e2164ec207f29afbd43676bfc2b
zoe <chaonbie@gmail.com>
2022-05-18 14:15:49 +0800

    Modify index.html.erb, own.html.erb and routes.rb

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::CategoriesController < Api::V1::BaseController
   def show
-    # render html: params
     products_json = Tag.find_by(name: params[:id]).products.to_json
     render json: products_json
   end
--- config/routes.rb
+++ config/routes.rb
@@ -44,6 +44,4 @@ Rails.application.routes.draw do
       end
     end
   end
-
-
 end
