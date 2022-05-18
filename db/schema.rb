f31212b334c73ce609e43b41f566d0d574e74b2d
livia <yamap19950521@gmail.com>
2022-05-18 14:08:33 +0800

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -79,13 +79,14 @@ ActiveRecord::Schema.define(version: 2022_05_11_024509) do
   end
 
   create_table "rooms", force: :cascade do |t|
-    t.string "countdown"
-    t.string "start_time"
+    t.string "life_time"
+    t.datetime "start_time"
     t.string "status"
-    t.string "level"
     t.integer "maxpeople"
+    t.bigint "product_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_rooms_on_product_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -113,4 +114,5 @@ ActiveRecord::Schema.define(version: 2022_05_11_024509) do
   add_foreign_key "records", "products"
   add_foreign_key "records", "rooms"
   add_foreign_key "records", "users"
+  add_foreign_key "rooms", "products"
 end
