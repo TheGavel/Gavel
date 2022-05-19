09c9fd9325e002c35c22b0c620fb3448f0f2fa3b
weian <kevin19960328@gmail.com>
2022-05-19 17:47:19 +0800

    complete bid actioncable

--- app/javascript/channels/room_channel.js
+++ app/javascript/channels/room_channel.js
@@ -13,6 +13,6 @@ consumer.subscriptions.create("RoomChannel", {
 
   received(data) {
     // Called when there's incoming data on the websocket for this channel
-    console.log(data);
+    document.querySelector("#output").innerHTML = data["bid"];
   },
 });
