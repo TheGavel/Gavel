1c271b523d340dd39c24a6c63b13dd683dc69d51
cin-yi-wei <m95162027@gmail.com>
2022-05-21 02:03:54 +0800

    Show seller avatar to product page

--- app/controllers/api/v1/products/base_controller.rb
+++ app/controllers/api/v1/products/base_controller.rb
@@ -1,12 +1,14 @@
 class Api::V1::Products::BaseController < ApplicationController
   def product_architecture(products)
     products.map{ |product|
-      { image: url_for(product.images[0]),
+      {
         name: product.name,
         description: product.description,
         status: product.status,
+        start_price: product.start_price,
         direct_price: product.direct_price,
-        start_price: product.start_price
+        seller_image: url_for(product.user.avatar),
+        product_image: url_for(product.images[0]),
       }
     }
   end
