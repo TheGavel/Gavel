18a5d8daae45806c5718a8e53651ea5e27696d0a
livia <yamap19950521@gmail.com>
2022-05-22 15:47:03 +0800

    fixed bug

--- app/javascript/controllers/autocomplete_controller.js
+++
@@ -1,8 +0,0 @@
-import { Application } from "stimulus"
-import { Autocomplete } from 'stimulus-autocomplete'
-import { definitionsFromContext } from "stimulus/webpack-helpers"
-
-const application = Application.start()
-const context = require.context("controllers", true, /_controller\.js$/)
-application.load(definitionsFromContext(context))
-application.register("autocomplete", Autocomplete)
--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -3,7 +3,9 @@
 
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
+import { Autocomplete } from 'stimulus-autocomplete'
 
 const application = Application.start()
 const context = require.context("controllers", true, /_controller\.js$/)
 application.load(definitionsFromContext(context))
+application.register("autocomplete", Autocomplete)
