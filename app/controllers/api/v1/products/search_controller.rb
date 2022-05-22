955a1c3f52fc296073e47898eb5ec6dd690e822a
livia <yamap19950521@gmail.com>
2022-05-22 16:02:54 +0800

    update edit_distance

--- app/controllers/api/v1/products/search_controller.rb
+++ app/controllers/api/v1/products/search_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::Products::SearchController <  Api::V1::Products::BaseController
     def show
         products = Product.search(params[:id],
-                misspellings: {edit_distance: 2}, page:1, per_page:30)
+                misspellings: {edit_distance: 5}, page:1, per_page:30)
                 render json: product_architecture(products)
     end
     
     def page
         products = Product.search(params[:search_id],
-        misspellings: {edit_distance: 2}, page: params[:page], per_page:30)
+        misspellings: {edit_distance: 5}, page: params[:page], per_page:30)
         render json: product_architecture(products)
     end
 end
