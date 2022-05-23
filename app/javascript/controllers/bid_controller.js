73bb9b4fda7b3d84a87192e6cd62ca4d2f6febb2
zzr1226sdo <yc117602@hotmail.com>
2022-05-23 19:19:22 +0800

    css fix

--- app/javascript/controllers/bid_controller.js
+++ app/javascript/controllers/bid_controller.js
@@ -1,13 +1,19 @@
 import { Controller } from "stimulus";
 export default class extends Controller {
-  static targets = ["record", "price"];
+  static targets = ["record", "bid", "input", "priceNow", "yourPrice"];
   show() {
-    if (this.recordTarget.style.display === "block") {
+    if (this.recordTarget.style.display === "flex") {
       this.recordTarget.style.display = "none";
-      this.priceTarget.style.display = "block";
+      this.bidTarget.style.display = "block";
     } else {
-      this.recordTarget.style.display = "block";
-      this.priceTarget.style.display = "none";
+      this.recordTarget.style.display = "flex";
+      this.bidTarget.style.display = "none";
     }
+  }
+  addPrice() {
+    console.log(this.yourPriceTarget.textContent);
+    console.log();
+    this.yourPriceTarget.textContent =
+      Number(this.priceNowTarget.textContent) + Number(this.inputTarget.value);
   }
 }
