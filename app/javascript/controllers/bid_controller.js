8325b380835ccd776ba4f684051284e66fa8132e
zzr1226sdo <yc117602@hotmail.com>
2022-05-24 17:36:24 +0800

    bid block complete

--- app/javascript/controllers/bid_controller.js
+++ app/javascript/controllers/bid_controller.js
@@ -2,18 +2,28 @@ import { Controller } from "stimulus";
 export default class extends Controller {
   static targets = ["record", "bid", "input", "priceNow", "yourPrice"];
   show() {
-    if (this.recordTarget.style.display === "flex") {
-      this.recordTarget.style.display = "none";
-      this.bidTarget.style.display = "block";
-    } else {
-      this.recordTarget.style.display = "flex";
+    this.inputTarget.value = "";
+    this.yourPriceTarget.textContent = this.priceNowTarget.textContent;
+    if (this.bidTarget.style.display === "block") {
       this.bidTarget.style.display = "none";
+    } else {
+      this.bidTarget.style.display = "block";
     }
   }
   addPrice() {
-    console.log(this.yourPriceTarget.textContent);
-    console.log();
     this.yourPriceTarget.textContent =
       Number(this.priceNowTarget.textContent) + Number(this.inputTarget.value);
+  }
+  plus100() {
+    this.inputTarget.value = Number(this.inputTarget.value) + 100;
+    this.addPrice();
+  }
+  plus500() {
+    this.inputTarget.value = Number(this.inputTarget.value) + 500;
+    this.addPrice();
+  }
+  plus1000() {
+    this.inputTarget.value = Number(this.inputTarget.value) + 1000;
+    this.addPrice();
   }
 }
