62d65f4b1a30d933b57924ee65ce78eb9f07134f
albert <albert3787@gmail.com>
2022-05-26 11:47:17 +0800

    replace flatpickr and add datetime-local input

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,4 +2,10 @@
 
 class Room < ApplicationRecord
   belongs_to :product
+  validates :start_time,:end_time ,presence: true
+  validates :maxpeople,presence:true ,numericality: { greater_than: 0,  only_integer: true }
+  validates_datetime :start_time, after: Time.now
+  validates_datetime :end_time, after: :start_time
+
+
 end
