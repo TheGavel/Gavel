a3074ab0d0dd1f53b0c5d9ac141b17716cbfc71c
albert <albert3787@gmail.com>
2022-05-26 13:41:36 +0800

    add romm model validates

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,9 +3,9 @@
 class Room < ApplicationRecord
   belongs_to :product
   validates :start_time,:end_time ,presence: true
-  validates :maxpeople,presence:true ,numericality: { greater_than: 0,  only_integer: true }
+  validates :maxpeople,presence:true ,numericality: {  greater_than_or_equal_to: 5,  only_integer: true }
   validates_datetime :start_time, after: Time.now
-  validates_datetime :end_time, after: :start_time
+  validates_datetime :end_time, after: :start_time , after: Time.now
 
 
 end
