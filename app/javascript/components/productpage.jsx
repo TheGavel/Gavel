f4d6b3e2c3e50000e46b393399bf1abdf5b3a5d7
livia <yamap19950521@gmail.com>
2022-05-28 14:48:03 +0800

    add_pull_down_menu

--- app/javascript/components/productpage.jsx
+++ app/javascript/components/productpage.jsx
@@ -4,9 +4,9 @@ import Bar from "./productClassificationBar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return (
-    <div className="bg-white relative z-1 mx-auto">
-      <h2 className="text-center text-3xl py-10">Explore Collections </h2>
+  return(
+    <div className="bg-white relative mx-auto  z-1">
+      <h2 className="text-center text-3xl py-6">Explore Collections </h2>
       <Router>
         <Bar />
         <div className="mx-auto py-6 px-4 sm:px-6 lg:px-8 max-w-2xl lg:max-w-[1420px] ">
