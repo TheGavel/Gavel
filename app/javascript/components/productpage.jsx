d6cda1a10f6674d2b40be62d3f02b2ba4d88e82a
livia <yamap19950521@gmail.com>
2022-05-29 23:04:18 +0800

    delete label and fix conflict

--- app/javascript/components/productpage.jsx
+++ app/javascript/components/productpage.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const App = () => {
   return(
     <div className="bg-white relative mx-auto z-1">
-      <h2 className="text-center text-3xl py-6">Explore Collections </h2>
+      <h2 className="text-center text-3xl py-10">Explore Collections </h2>
       <Router>
         <Bar />
         <div className="mx-auto py-6 px-4 sm:px-6 lg:px-8 max-w-2xl lg:max-w-[1420px] ">
