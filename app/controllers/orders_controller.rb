433a2071bf30ced2b884edc116f1c70a928b97b3
albert <albert3787@gmail.com>
2022-05-30 17:10:22 +0800

    add buyerlist and sellitem

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,14 +1,20 @@
 class OrdersController < ApplicationController
   protect_from_forgery
-  skip_before_action :require_login, only: %i[return_response]
+  skip_before_action :require_login
+
 
   def record
     @records = current_user.orders.all
   end
 
+
   def check
     @order = Order.find(params[:id])
     @form_info = Newebpay::Mpg.new(@order).form_info
+    @MerchantID = @form_info[:MerchantID]
+    @TradeInfo = @form_info[:TradeInfo]
+    @TradeSha = @form_info[:TradeSha]
+    @Version = @form_info[:Version]
   end
 
   def return_response
@@ -16,10 +22,10 @@ class OrdersController < ApplicationController
     order= Order.find(response.result["MerchantOrderNo"])
     if response.status == "SUCCESS"
       order.pay!
-      redirect_to own_products_path , notice: "已付款成功"
+      redirect_to root_path , notice: "已付款成功"
     else
       order.fail!
-      redirect_to own_products_path , alert: "付款失敗"
+      redirect_to root_path , alert: "付款失敗"
     end
   end
 end
