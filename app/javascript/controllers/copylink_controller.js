af3b6b436eaa692ef66bb6c1fe3ffc2f0925e21e
zzr1226sdo <yc117602@hotmail.com>
2022-05-30 16:53:41 +0800

    page css modified

--- app/javascript/controllers/copylink_controller.js
+++ app/javascript/controllers/copylink_controller.js
@@ -1,19 +1,17 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["link"]
+  static targets = ["link"];
   copy(e) {
     e.preventDefault();
     const copyText = `${e.target.dataset.url}`;
     navigator.clipboard
       .writeText(copyText)
-      .then((data) => {
-        console.log(data);
-        // this.linkTarget
-        // alert("已複製房間連結");
-      })
+      .then((data) => {})
       .catch((err) => {
         console.log(err);
       });
+
+    this.element.textContent = "已複製！";
   }
 }
