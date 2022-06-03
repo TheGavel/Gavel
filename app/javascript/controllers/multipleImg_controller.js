70609edbeb096e489e4ca03f2e6d0744f96a7b4f
livia <yamap19950521@gmail.com>
2022-06-03 12:56:39 +0800

    update product launch css

--- app/javascript/controllers/multipleImg_controller.js
+++ app/javascript/controllers/multipleImg_controller.js
@@ -29,7 +29,7 @@ export default class extends Controller {
         "label": "選擇照片:"
       },
       "images": {
-        "baseImage": "https://ch-image-resizer.cwg.tw/resize/uri/https%3A%2F%2Fas.chdev.tw%2Fweb%2Farticle%2F9%2Ff%2F4%2F903276c8-ff55-4e63-b49c-819b3d6c8de0%2FA1105542.jpg/?w=621"
+        "baseImage": ""
       }
       // ,presetFiles: ["https://s.yimg.com/ny/api/res/1.2/SjlAHKRRZKeGTNX7z6yFew--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQyNg--/https://s.yimg.com/uu/api/res/1.2/1yT2e7805ymUEM6d5ednCA--~B/aD01MzM7dz04MDA7YXBwaWQ9eXRhY2h5b24-/https://media.zenfs.com/en/nownews.com/372097716213d50c273ccda4594fdee0"]
 
