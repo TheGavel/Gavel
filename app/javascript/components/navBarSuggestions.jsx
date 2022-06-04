0b1615780eb843bb0377032b581328688163730a
zoe <chaonbie@gmail.com>
2022-06-04 17:07:10 +0800

    Change CSS style of React navbar & search

--- app/javascript/components/navBarSuggestions.jsx
+++ app/javascript/components/navBarSuggestions.jsx
@@ -1,78 +1,93 @@
 import React from "react";
-import Rails from "@rails/ujs"
+import Rails from "@rails/ujs";
 import { Link } from "react-router-dom";
-import Autosuggest from 'react-autosuggest'
-import { debounce } from 'throttle-debounce'
-import "../css/suggestion"
+import Autosuggest from "react-autosuggest";
+import { debounce } from "throttle-debounce";
+import "../css/suggestion";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import {
+  solid,
+  regular,
+  brands,
+} from "@fortawesome/fontawesome-svg-core/import.macro";
 
 class navBarSuggestions extends React.Component {
   state = {
-    value: '',
-    suggestions: []
-  }
+    value: "",
+    suggestions: [],
+  };
 
   componentWillMount() {
     this.onSuggestionsFetchRequested = debounce(
       500,
       this.onSuggestionsFetchRequested
-    )
+    );
   }
 
-  renderSuggestion = suggestion => {
+  renderSuggestion = (suggestion) => {
     return (
       <div className="result">
-        <div className="bg-gradient-to-r from-cyan-500 to-blue-500">{suggestion}</div>
+        <div className="">{suggestion}</div>
       </div>
-    )
-  }
+    );
+  };
 
   onChange = (event, { newValue }) => {
-    this.setState({ value: newValue })
-  }
+    this.setState({ value: newValue });
+  };
 
   onSuggestionsFetchRequested = ({ value }) => {
-    console.log( "value",value );
+    console.log("value", value);
     Rails.ajax({
-              url: `/api/v1/products/search/${encodeURI(value)}`,
-              type: 'GET',
-              data: JSON,
-              success: resp => {
-                console.log(resp);
-                const results = resp.map(h => h.name)
-                console.log(results);
-                this.setState({ suggestions: results })
-              }
-            })
-  }
+      url: `/api/v1/products/search/${encodeURI(value)}`,
+      type: "GET",
+      data: JSON,
+      success: (resp) => {
+        console.log(resp);
+        const results = resp.map((h) => h.name);
+        console.log(results);
+        this.setState({ suggestions: results });
+      },
+    });
+  };
 
   onSuggestionsClearRequested = () => {
-    this.setState({ suggestions: [] })
-  }
+    this.setState({ suggestions: [] });
+  };
 
   render() {
-    const { value, suggestions } = this.state
+    const { value, suggestions } = this.state;
     const inputProps = {
-      placeholder: '搜尋產品',
+      placeholder: "輸入您想找的商品",
       value,
-      onChange: this.onChange
-    }
+      onChange: this.onChange,
+    };
 
     return (
-      <div className="text-center inline-block grow-0">
-        <Autosuggest className="inline-block"
+      <div className="flex items-center">
+        <Autosuggest
+          className="inline-block"
           suggestions={suggestions}
           onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
           onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-          getSuggestionValue={suggestion => suggestion}
+          getSuggestionValue={(suggestion) => suggestion}
           renderSuggestion={this.renderSuggestion}
           inputProps={inputProps}
         />
-        <Link key={"SearchLink"} className="search_icon inline-block"
-          to={`/products/search/${encodeURI(value)}`}>🔍️
+        <Link
+          key={"SearchLink"}
+          className="search_icon inline-block"
+          to={`/products/search/${encodeURI(value)}`}
+        >
+          <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            className="text-white m-auto"
+          />
         </Link>
       </div>
-    )
+    );
   }
 }
 
-export default navBarSuggestions
+export default navBarSuggestions;
