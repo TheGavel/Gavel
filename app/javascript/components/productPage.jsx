0a5de2d538e2e52137fad95b3e6f55ef2183de26
zoe <chaonbie@gmail.com>
2022-06-04 21:27:28 +0800

    Modify CSS of homepage

--- app/javascript/components/productPage.jsx
+++ app/javascript/components/productPage.jsx
@@ -7,11 +7,9 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 const Productpage = ({ props }) => {
   console.log("props.policy", props.policy);
   return (
-    <div className="bg-blue-50 relative mx-auto z-1 ">
+    <div className="bg-blue-50 relative mx-auto z-1 mb-8">
       <Router>
         <NavBar props={props} />
-        {/* <h2 className="text-center text-3xl py-6 mt-14">Explore Collections </h2> */}
-
         <CategoryBar />
         <div className="mx-auto py-2 px-4 sm:px-6 lg:px-8 max-w-2xl lg:max-w-[1420px] z-20">
           <div className="relative mt-6 grid gridfit-[18rem]  gap-y-12 gap-x-8">
