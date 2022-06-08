4e1a06e31fa4ab96f4caa1260a8863d01d536367
zzr1226sdo <yc117602@hotmail.com>
2022-06-08 12:03:23 +0800

    render bid ranking once entering the room

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -18,8 +18,8 @@ class RoomsController < ApplicationController
 
   def show
     @product = Product.find(params[:id])
-    # @url = "#{root_url}rooms/#{params[:id]}"
     @url = request.host + "/rooms/" + params[:id]
+    @bid = [@product.records.last]
   end
 
   def edit
