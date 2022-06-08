3ee7f75bd6c8fd3d666dbf4155b2889aab8d5e6e
cin-yi-wei <m95162027@gmail.com>
2022-06-08 17:07:00 +0800

    only search product status equal publish

--- app/worker/broadcast_endtime_worker.rb
+++ app/worker/broadcast_endtime_worker.rb
@@ -6,7 +6,7 @@ class BroadcastEndtimeWorker
     highest_record = Record.where(room_id: room_id).last
     Boughtlist.create( user_id: highest_record.user.id ,
                        product_id: highest_record.product.id)
-
+    Product.find(room_id).sold!
     Order.create( product_id: room_id, price: highest_record.bid, email: highest_record.user.email, description: highest_record.product.name , buyer_id: highest_record.user.id, seller_id: highest_record.product.user.id)
 
     ActionCable.server.broadcast "Bid:#{room_id}",
